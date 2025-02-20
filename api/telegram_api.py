19b90affbc7731f7a3971afb68ad93f8b19c6ebe
Nico Hoch <nico.hoch1@web.de>
2025-02-20 21:17:57 +0100

    first steps for summary

---
+++ api/telegram_api.py
@@ -0,0 +1,8 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filter,
+    ContextTypes,
+)
--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ app = func.FunctionApp()
 
 
 @app.timer_trigger(
-    schedule="*/10 * * * * *",  # This schedule will run every 10 seconds
+    schedule="0 */6 * * *",  # This schedule will run every 6 hours
     arg_name="myTimer",
     run_on_startup=False,
     use_monitor=False,
@@ -18,3 +18,13 @@ def check_emails(myTimer: func.TimerRequest) -> None:
     logging.info("Python timer trigger function executed.")
 
     main()
+
+
+@app.timer_trigger(
+    schedule="0 18,6 * * *",  # This schedule will run every day at 6 AM and 6 PM
+    arg_name="myTimer",
+    run_on_startup=False,
+    use_monitor=False,
+)
+def send_summary(myTimer: func.TimerRequest) -> None:
+    print("summary")
